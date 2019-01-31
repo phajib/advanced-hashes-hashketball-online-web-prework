45f8e0fbefebc15c96b2746d452ff71eba9a105e
H B <phajib@gmail.com>
2019-01-31 07:13:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -252,8 +252,8 @@ def player_with_longest_name
   playerName = nil
   game_hash.each do |location, team_data|
     team_data[:players].each do |player_name, player_data|
-      if player_data[:player_name] > playerName
-        playerName = player_data[:player_name].char.length
+      if player_name > playerName
+        playerName = player_name.char.length
         player =  player_name
       end
     end
